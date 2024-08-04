f0073c6774f0e0982c3d011a18d372199ae3d7c3
shubham raj <aassuhi@proton.me>
2024-08-04 14:50:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("12557431"))
+API_HASH = getenv("dbf930927f239982765d29cf6cb9a579")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+BOT_TOKEN = getenv("7029065576:AAHReOT0L-nZDYdB2nlrqbRHkmfKVwZwYek")
+MONGO_DB_URI = getenv("mongodb+srv://jaypck:ey8ZyUVRcKiiiO3A@cluster0.q7zla97.mongodb.net/?retryWrites=true&w=majority", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
+OWNER_ID = int(getenv("OWNER_ID", 6724898708)
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+Nh3kNYQawB8zN2Nk")
